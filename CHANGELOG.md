af3ffaabc8a73811abf91012a69d4f52545626ec
leonardo <pintoleonardo1023@gmail.com>
2023-12-30 20:36:00 -0500

    build: :construction_worker: se actualizaron algunas dependencias y se está consumiendo una api
    Jurassic App

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,4 @@
-## 0.0.0 (2023-05-30)
+## 0.0.1 (2023-05-30)
 
 * build: :building_construction: eliminé algunas dependencias ([f428542](https://github.com/leofadev/JurassicApp/commit/f428542))
 * build: :construction_worker: se agregaron algunas dependencias ([f374e0f](https://github.com/leofadev/JurassicApp/commit/f374e0f))
--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import  { useEffect, useState } from 'react';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    // Usa fetch() para hacer una solicitud GET a tu API
+    fetch('https://script.google.com/macros/s/AKfycby0crc4kbWAiynTe6DOGrtICHSG5WOOG2rb0erD4vTy7OznrAZdoDYiTsU4sggPXVvA/exec')
+      .then(response => response.json())
+      .then(data => setData(data))
+      .catch(error => console.error('Error:', error));
+
+  }, []); // El array vacío como segundo argumento indica que este efecto se ejecutará solo una vez después de la renderización inicial.
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="App">
+      <h1>Datos de Dinosaurios</h1>
+      <ul>
+        {data.map((dinosaurio, index) => (
+          <li key={`${dinosaurio.name}-${dinosaurio.Especie}-${dinosaurio["Name Movie"]}-${dinosaurio.type}-${index}`}>
+            {dinosaurio["Name Movie"]} - {dinosaurio.name} - {dinosaurio.Especie} - {dinosaurio.type}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
-export default App
+export default App;
+
