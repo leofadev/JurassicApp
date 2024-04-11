2515e25c113db18249d322efe608441d5730aa53
leonardo <pintoleonardo1023@gmail.com>
2024-04-10 21:51:55 -0500

    feat: :sparkles: se modificó el código para el consumo de la api, esta ultima actualizada
    JurassicWeb

--- src/App.jsx
+++ src/App.jsx
@@ -5,24 +5,29 @@ function App() {
   const [data, setData] = useState([]);
 
   useEffect(() => {
-    // Usa fetch() para hacer una solicitud GET a tu API
-    fetch('https://script.google.com/macros/s/AKfycby0crc4kbWAiynTe6DOGrtICHSG5WOOG2rb0erD4vTy7OznrAZdoDYiTsU4sggPXVvA/exec')
+    fetch('https://script.google.com/macros/s/AKfycbydyRvTTgmNfBjmTMWnS3NbeaTSmmR5eVtVTo03K36U46ukShua2sXkM_6IGByAQfI/exec')
       .then(response => response.json())
-      .then(data => setData(data))
+      .then(result => setData(result.data)) // Asegúrate de acceder a la propiedad 'data' si es necesario
       .catch(error => console.error('Error:', error));
-
-  }, []); // El array vacío como segundo argumento indica que este efecto se ejecutará solo una vez después de la renderización inicial.
+  }, []);
+   // El array vacío como segundo argumento indica que este efecto se ejecutará solo una vez después de la renderización inicial.
 
   return (
     <div className="App">
       <h1>Datos de Dinosaurios</h1>
-      <ul>
+      <div className="card-container">
         {data.map((dinosaurio, index) => (
-          <li key={`${dinosaurio.name}-${dinosaurio.Especie}-${dinosaurio["Name Movie"]}-${dinosaurio.type}-${index}`}>
-            {dinosaurio["Name Movie"]} - {dinosaurio.name} - {dinosaurio.Especie} - {dinosaurio.type}
-          </li>
+          <div key={`${dinosaurio.name}-${dinosaurio.Especie}-${dinosaurio["Name Movie"]}-${dinosaurio.type}-${index}`} className="card">
+            <img className='img-thumbnail' src={dinosaurio.image} alt={dinosaurio.name} />
+            <div className="card-content">
+              <h3>{dinosaurio.name}</h3>
+              <p>Especie: {dinosaurio.Especie}</p>
+              <p>Tipo: {dinosaurio.type}</p>
+              <p>Película: {dinosaurio["Name Movie"]}</p>
+            </div>
+          </div>
         ))}
-      </ul>
+      </div>
     </div>
   );
 }
